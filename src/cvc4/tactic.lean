f6142d6b05c370b7e055ccac85e7497efbfac3b6
Zesen Qian <git@riaqn.org>
2018-08-12 18:14:52 -0400

    my job is almost done

--- src/cvc4/tactic.lean
+++ src/cvc4/tactic.lean
@@ -10,40 +10,39 @@ import .reflect
 
 def b2p := reflect.p_app
 
-meta def prop_to_formula  : list declare → expr → tactic term := λ d t, match t with
-| `(%%(t0) → %%(t1)) := term.app "=>" <$> monad.sequence [prop_to_formula d t0, prop_to_formula (declare.mk "" "" :: d) t1]
-| `(%%(t0) ∧ %%(t1)) := term.app "and" <$> monad.sequence [prop_to_formula d t0, prop_to_formula d t1]
-| `(%%(t0) ∨ %%(t1)) := term.app "or" <$> monad.sequence [prop_to_formula d t0, prop_to_formula d t1]
-| `(¬ %%(t')) := term.app "not" <$> monad.sequence [prop_to_formula d t']
-| `(coe_sort %%(t')) := prop_to_formula d t'
+meta def prop_to_formula  : expr → tactic term := λ t, match t with
+| `(%%(t0) → %%(t1)) := term.app "=>" <$> monad.sequence [prop_to_formula t0, prop_to_formula t1]
+| `(%%(t0) ∧ %%(t1)) := term.app "and" <$> monad.sequence [prop_to_formula t0, prop_to_formula t1]
+| `(%%(t0) ∨ %%(t1)) := term.app "or" <$> monad.sequence [prop_to_formula t0, prop_to_formula t1]
+| `(¬ %%(t')) := term.app "not" <$> monad.sequence [prop_to_formula t']
+| `(coe_sort %%(t')) := prop_to_formula t'
 | `(false) := return $ term.symbol "false"
 | `(true) := return $ term.symbol "true"
-| expr.local_const n m bi ty := return $ term.symbol n.to_string
-| expr.var i := match d.nth i with
-                | option.none := tactic.fail ("unbound", i)
-                | option.some k := return $ term.symbol k.symbol
-                end
+| expr.local_const n m bi ty := return $ term.symbol m.to_string
 | _ := tactic.fail ("unrecognized prop", t)
 end
 
-meta def target_to_query : list declare → expr → tactic query := λ d t, match t with
+meta def target_to_query : expr → tactic query := λ t, match t with
 | expr.pi n bi ty t' := do sr ← match ty with
                            | `(bool) := return $ option.some "Bool"
                            | _ := do return $ option.none
                            end,
                            match sr with
                            | option.some sr := do _ ← tactic.intro n,
-                                                  target_to_query (declare.mk n.to_string sr :: d) t'
-                           | option.none := do k ← prop_to_formula d t,
-                                               return $ query.mk d.reverse [term.app "not" [k]]
+                                                  v ← tactic.mk_local' n bi ty,
+                                                  let t' := t'.instantiate_var v,
+                                                  k ← target_to_query t',
+                                                  return $ query.mk ((declare.mk n.to_string sr) :: k.declares) k.asserts
+                           | option.none := do k ← prop_to_formula t,
+                                               return $ query.mk [] [term.app "not" [k]]
                            end
-| _ := do k ← prop_to_formula d t,
-          return $ query.mk d.reverse [term.app "not" [k]]
+| _ := do k ← prop_to_formula t,
+          return $ query.mk [] [term.app "not" [k]]
 end
 
 meta def prove : tactic unit :=
 do tg ← tactic.target,
-   q ← target_to_query [] tg,
+   q ← target_to_query tg,
    r ← tactic.unsafe_run_io $ solve q,
    match r with
    | except.error e := tactic.fail e
